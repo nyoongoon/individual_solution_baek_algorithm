5a5fe8629cf31630b92300b0123b1877e0a1c3ab
gardenlee96 <gardenlee96@gmail.com>
2021-07-06 00:09:05 +0900

    stage4

---
+++ LSW/part01/10869.py
@@ -0,0 +1,8 @@
+#! /opt/homebrew/bin/python3.9
+input_value=input()
+input_value=input_value.split()
+print(int(input_value[0])+int(input_value[1]))
+print(int(input_value[0])-int(input_value[1]))
+print(int(input_value[0])*int(input_value[1]))
+print(int(int(input_value[0])/int(input_value[1])))
+print(int(input_value[0])%int(input_value[1]))
---
+++ LSW/part01/2588.py
@@ -0,0 +1,13 @@
+#! /opt/homebrew/bin/python3.9
+
+A=input()
+B=input()
+
+first=int(A)*int(B[2])
+second=int(A)*int(B[1])
+third=int(A)*int(B[0])
+
+print(first)
+print(second)
+print(third)
+print(int(A)*int(B))
