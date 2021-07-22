5cd9e2942a3e7d9961321d6ea09fe18aeb32a240
HoyeongJeon <hoyeong0302@gmail.com>
2021-07-22 16:36:23 +0900

    210722 16:36

--- JHY/C_Algorithm/C_Algorithm/lv8/10757.c
+++ JHY/C_Algorithm/C_Algorithm/lv8/10757.c
@@ -1,1 +1,9 @@
+#include <stdio.h>
 
+int main(void)
+{
+    char a, b;
+    scanf("%c %c", &a, &b);
+    printf("%c\n", a + b);
+    return 0;
+}
--- JHY/C_Algorithm/C_Algorithm/lv9/2581.c
+++ JHY/C_Algorithm/C_Algorithm/lv9/2581.c
@@ -2,14 +2,29 @@
 
 int main(void)
 {
-    int N,M;
-    scanf("%d %d", &N, &M);
-    for(int i = 2; i < N; i++)
+    int N, M, i = 0, l = 0, sum = 0, order = 0, min = 0; // order 순서.
+    scanf("%d %d", &N, &M);
+    for(i = N; i <= M; i++)
     {
-        for(int l = 1; l < i; l++)
+        for(l = 2; l < i; l++)
         {
-            if(i % l != 0)
+            if(i % l == 0) //소수는 1과 자기자신을 제외하곤 나뉘어지면 안됨! 즉 중간에 나뉘어지는게 있으면 소수가 아니므로 break!
+                break;
         }
+        if(l == i)
+        {
+            order++; // 소수가 하나 나올때마다 숫자를 증가시킴. 즉 첫번째 나온 소수가 최솟값!
+            sum += l;
+            if(order == 1)
+                min = l;
+        }
+    }
+    if(order == 0) // 소수가 나올 때 마다 order를 증가시키는데, order가 0이면 소수가 하나도 안나왔다는 말.
+        printf("-1\n");
+    else
+    {
+        printf("%d\n", sum);
+        printf("%d\n", min);
     }
     return 0;
 }
