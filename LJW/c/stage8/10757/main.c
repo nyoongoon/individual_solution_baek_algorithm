f77d0ac017e06c331f09fe143eb79778c9c030cb
gardenlee96 <gardenlee960828@gmail.com>
2021-07-22 18:06:17 +0900

    20210722_1906

--- LJW/c/stage8/10757/main.c
+++ LJW/c/stage8/10757/main.c
@@ -1,85 +1,94 @@
 #include <stdio.h>
 #include <string.h>
 
+char* reverse(char* string){
+    int n;
 
-char* reverse(char* string,int len){
-    char temp[len];
+       n =  strlen(string);
 
-    strcpy(temp,string);
+       char temp[n+1];
 
-  for (int i = 0; i < len; i++)
-  {
-     string[i] = temp[len - 1 -i]; 
-  }
-return string;  
+            strcpy(temp,string);
+            for (int i = 0; i < n; i++)
+            {
+                string[i] = temp[n - 1 - i];//배열 자리수 변환
+            }
+
+            return string;
 }
 
+char* Add_string(char* string1,char* string2)환
+    int n1,n2,n_sum;
+    char sum_string[10002];
+
+        string1 = reverse(string1); //입력받은 배열 자리수 바꾸
+        string2 = reverse(string2);기
 
 
-char* add(char* string1,char* string2,int len){
-   char sum[len+1]; 
-    
-    for (int i = 0; i < len; i++)
-    {
-       if(strlen(string1) == strlen(string2)){
-           sum[i] = string1[i] + string2[i] - '0';
-       } 
-       
-       else if(strlen(string1) > strlen(string2)){
-           if(i < strlen(string2))
-           sum[i] = string1[i] + string2[i] - '0';
+        n1 = strlen(string1);
+        n2 = strlen(string2);
 
-            sum[i] = string1[i];     
-       }
+                if(n1 == n2){//자리수가 같은 경우 각자릿수 더하기
+                for (int i = 0; i < n1; i++)
+                {
+                    sum_string[i] = string1[i] + string2[i] - '0';
+                }
+                }
 
-       else{
-           if(i < strlen(string1))
-           sum[i] = string1[i] + string2[i] - '0';
-            sum[i] = string2[i];     
-       }
-    }  
-        
-    for (int i= 0; i < len; i++)
-    {
-        if(sum[i] >= '10'){
-            sum[i+1] += 1;
-            sum[i] -= '10'; 
-         }
-    }
-   if(sum[len-1] == 1){
-        sum[len -1] += '0'; 
-        len ++;
-   }
-    return reverse(sum,len);
+
+                    else if(n1 > n2) //자리수가 다를경우 각 자릿수 더하기
+                        for (int i = 0; i < n2; i++)
+                        {
+                            sum_string[i] = string1[i] + string2[i] - '0';
+                        }
+
+                        for (int i = n2; i < n1; i++)
+                        {
+                        sum_string[i] = string1[i]; 
+                        }
+                    }
+
+
+                        else if(n2 > n1){//자리수가 다를경우 각 자릿수 더하기
+                            for (int i = 0; i < n1; i++)
+                            {
+                                sum_string[i] = string1[i] + string2[i] - '0';
+                            }
+
+                            for (int i = n1; i < n2; i++)
+                            {
+                            sum_string[i] = string2[i];환
+                            }
+                        }
+                
+        n_sum = strlen(sum_string);
+
+
+
+            for (int i = 0; i < n_sum; i++)// 자릿수가 10 이 넘는것들은 다음 자릿수에 1 더해주기
+            {
+                if(sum_string[i] > '9'){
+                    sum_string[i+1] += 1;
+                    sum_string[i]-=10;
+                }
+            }
+
+                    if(sum_string[n_sum] == 1)//첫번째 자리수가 올림 당할때 아스키코드로 변환
+                        sum_string[n_sum] += '0';
+                    
+                        printf("%s\n",reverse(sum_string));//출력
+
+            return 0;
 }
-
 
 int main(){
-    char string1[10001],string2[10002];
-    int len,n1,n2;
-
-    scanf("%s %s",string1,string2);
-
-    n1 = strlen(string1);
-    n2 = strlen(string2);
+    char string1[10001];
+    char string2[10002];
 
 
-    if(n1 >= n2)
-        len = strlen(string1);
-    else 
-        len = strlen(string2); 
-    
-    for (int i = 0; i < n1; i++)
-    {
-        string1[i] -= '0';
-    }
-    
-for (int i = 0; i < n2; i++)
-    {
-        string2[i] -= '0';
-    }
+        scanf("%s %s",string1,string2);
 
-    printf("%s",add(reverse(string1,n1),reverse(string2,n2),len));
+        Add_string(string1,string2);
 
-    return 0;
+        return 0;
 }
