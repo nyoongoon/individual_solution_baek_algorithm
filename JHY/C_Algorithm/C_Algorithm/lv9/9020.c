e7bbc4d6db46bd8d4f5b3303ac1b2b544c5af67e
HoyeongJeon <hoyeong0302@gmail.com>
2021-07-24 15:52:49 +0900

    DATE:2021-07-24 15:52 USER:JHY

---
+++ JHY/C_Algorithm/C_Algorithm/lv9/9020.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+int prime(int number); //소수면 return 1 , 소수 아니면 return 0;
+
+int main(void)
+{
+    int number, a, i, times;
+    scanf("%d", &times); // 몇 번 돌릴지...
+    for(int j = 0; j < times; j++)
+    {
+        scanf("%d", &number);
+        a = number / 2;
+        if(prime(a) == 1) // 절반이 바로 소수인 경우
+            printf("%d %d\n", a, a);
+        else
+        {
+            for(i = 1 ; i < a; i++)
+            {
+                if(prime(a-i) == 1 && prime(a + i) == 1) // 절반에서 숫자 1씩더하고, 빼면서 두 수 모두 소수가 될 때 까지 찾는 알고리즘.
+                {
+                    printf("%d %d\n", a-i, a + i);
+                    break;
+                }
+            }
+        }
+    }
+    return 0;
+}
+
+int prime(a)
+{
+    int i;
+    for(i = 2; i < a; i++)
+    {
+        if(a % i == 0)
+            break;
+    }
+    if(a == i)
+        return 1;
+    else
+        return 0;
+}
