31c98365147edb7587d5376f294db10097abdf2a
HoyeongJeon <hoyeong0302@gmail.com>
2021-07-29 23:25:54 +0900

    DATE:2021-07-29 23:25 USER:JHY

--- JHY/C_Algorithm/C_Algorithm/lv9/9020.c
+++ JHY/C_Algorithm/C_Algorithm/lv9/9020.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-int prime(int number); //소수면 return 1 , 소수 아니면 return 0;
+#include <stdbool.h>
+
+bool prime(int number); //소수면 return 1 , 소수 아니면 return 0;
 
 int main(void)
 {
@@ -9,13 +11,13 @@ int main(void)
     {
         scanf("%d", &number);
         a = number / 2;
-        if(prime(a) == 1) // 절반이 바로 소수인 경우
+        if(prime(a) == true) // 절반이 바로 소수인 경우
             printf("%d %d\n", a, a);
         else
         {
             for(i = 1 ; i < a; i++)
             {
-                if(prime(a-i) == 1 && prime(a + i) == 1) // 절반에서 숫자 1씩더하고, 빼면서 두 수 모두 소수가 될 때 까지 찾는 알고리즘.
+                if(prime(a-i) == true && prime(a + i) == true) // 절반에서 숫자 1씩더하고, 빼면서 두 수 모두 소수가 될 때 까지 찾는 알고리즘.
                 {
                     printf("%d %d\n", a-i, a + i);
                     break;
@@ -26,7 +28,7 @@ int main(void)
     return 0;
 }
 
-int prime(a)
+bool prime(a) //소수 찾는 함수.
 {
     int i;
     for(i = 2; i < a; i++)
@@ -35,7 +37,7 @@ int prime(a)
             break;
     }
     if(a == i)
-        return 1;
+        return true;
     else
-        return 0;
+        return false;
 }
